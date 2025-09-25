9d6bd8ae8209e6063ffdedf64d59e360b4fb9e02
Mohamed Zahran <work.mohamed.zahran.05@gmail.com>
2025-09-25 23:18:45 +0300

    Added proper articles table and profiles as well

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('password');
             $table->string('role')->default('user');
             $table->foreignId('profile_id')->nullable();
+            $table->rememberToken();
             $table->timestamps();
         });
 
